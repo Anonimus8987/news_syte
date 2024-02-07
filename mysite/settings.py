5f0164cb770235c71b112f0f75b90e1536e35000
Anonimus8987 <abdulazizsobitov2004@gmail.com>
2024-02-07 23:56:08 +0500

    qweqwe

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,14 +82,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'django',
-        # 'USER': 'root',
-        # 'PASSWORD': '',
-        # 'HOST': 'localhost',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_site',
+        'USER': 'postgres',
+        'PORT': 5432,
+        'PASSWORD': 'poker3301',
+        'HOST': 'localhost'
     }
 }
 
